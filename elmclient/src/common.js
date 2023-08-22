186ae6f5234b12ece811340157ed36ff29005fb9
Sisiliu312 <101872688+Sisiliu312@users.noreply.github.com>
2023-08-23 00:39:27 +0800

    common.js

---
+++ elmclient/src/common.js
@@ -0,0 +1,50 @@
+//获取当前时间（xxxx-xx-xx）
+export function getCurDate(){
+	var now = Date();
+	var year = now.getFullYear();
+	var month = now.getMonth()+1;
+	var day = now.getDate();
+	month = month < 10 ? "0" + month : month;
+	day = day < 10 ? "0" + day : day;
+	return year + "-" + month + "-" + day;
+}
+
+//向sessionStorage中存储一个JSON对象
+export function setSessionStorage(keyStr, value){
+	sessionStorage.setItem(keyStr, JSON.stringify(value));
+}
+
+//从setsessionStorage中获取一个JSON对象（取不到时返回null）
+export function getSessionStorage(keyStr){
+	var str = sessionStorage.getItem(keyStr);
+	if(str == '' || str == null || str == 'null' || str == undefined){
+		return null;
+	}else{
+		return JSON.parse(str);
+	}
+}
+
+//从sessionStorage中移除一个JSON对象
+export function removeSessionStorage(keyStr){
+	sessionStorage.removeItem(keyStr);
+}
+
+//向localStorage中存储一个JSON对象
+export function setLocalStorage(keyStr){
+	localStorage.setItem(keyStr, JSON.stringify(value));
+}
+
+//从slocalStorage中获取一个JSON对象（取不到时返回null）
+export function getLocalStorage(keyStr){
+	var str = localStorage.getItem(keyStr);
+	if(str == '' || str == null || str == 'null' || str == undefined){
+		return null;
+	}else{
+		return JSON.parse(str);
+	}
+}
+
+//从localStorage中移除一个JSON对象
+export function removeLocalStorage(keyStr){
+	localStorage.removeItem(keyStr);
+}
