7bdc53891c9aa56b6e18b1aa57ad0d5f20a5ce63
Sisiliu312 <101872688+Sisiliu312@users.noreply.github.com>
2023-08-24 17:32:19 +0800

    首页完成

--- elmclient/src/common.js
+++ elmclient/src/common.js
@@ -1,8 +1,8 @@
 //获取当前时间（xxxx-xx-xx）
-export function getCurDate(){
+export function getCurDate() {
 	var now = Date();
 	var year = now.getFullYear();
-	var month = now.getMonth()+1;
+	var month = now.getMonth() + 1;
 	var day = now.getDate();
 	month = month < 10 ? "0" + month : month;
 	day = day < 10 ? "0" + day : day;
@@ -10,41 +10,41 @@ export function getCurDate(){
 }
 
 //向前端sessionStorage中存储一个JSON对象
-export function setSessionStorage(keyStr, value){
+export function setSessionStorage(keyStr, value) {
 	sessionStorage.setItem(keyStr, JSON.stringify(value));
 }
 
 //从setsessionStorage中获取一个JSON对象（取不到时返回null）
-export function getSessionStorage(keyStr){
+export function getSessionStorage(keyStr) {
 	var str = sessionStorage.getItem(keyStr);
-	if(str == '' || str == null || str == 'null' || str == undefined){
+	if (str == '' || str == null || str == 'null' || str == undefined) {
 		return null;
-	}else{
+	} else {
 		return JSON.parse(str);
 	}
 }
 
 //从sessionStorage中移除一个JSON对象
-export function removeSessionStorage(keyStr){
+export function removeSessionStorage(keyStr) {
 	sessionStorage.removeItem(keyStr);
 }
 
 //向永久存储localStorage中存储一个JSON对象
-export function setLocalStorage(keyStr){
+export function setLocalStorage(keyStr) {
 	localStorage.setItem(keyStr, JSON.stringify(value));
 }
 
 //从slocalStorage中获取一个JSON对象（取不到时返回null）
-export function getLocalStorage(keyStr){
+export function getLocalStorage(keyStr) {
 	var str = localStorage.getItem(keyStr);
-	if(str == '' || str == null || str == 'null' || str == undefined){
+	if (str == '' || str == null || str == 'null' || str == undefined) {
 		return null;
-	}else{
+	} else {
 		return JSON.parse(str);
 	}
 }
 
 //从localStorage中移除一个JSON对象
-export function removeLocalStorage(keyStr){
+export function removeLocalStorage(keyStr) {
 	localStorage.removeItem(keyStr);
 }
--- elmclient/src/main.js
+++ elmclient/src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
+import {
+	createApp
+} from 'vue'
 import App from './App.vue'
 import router from './router'
 
@@ -17,27 +19,44 @@ import {
 
 //设置axios的基础url部分
 axios.defaults.baseURL = 'http://localhost:8081/elm/';
+
+//原vue2代码
 //将axios挂载到vue实例上，使用时就可以this.$axios这样使用了
-Vue.prototype.$axios = axios;
+// Vue.prototype.$axios = axios;
+// Vue.prototype.$getCurDate = getCurDate;
+// Vue.prototype.$setSessionStorage = setSessionStorage;
+// Vue.prototype.$getSessionStorage = getSessionStorage;
+// Vue.prototype.$removeSessionStorage = removeSessionStorage;
+// Vue.prototype.$setLocalStorage = setLocalStorage;
+// Vue.prototype.$getLocalStorage = getLocalStorage;
+// Vue.prototype.$removeLocalStorage = removeLocalStorage;
 
-Vue.prototype.$getCurDate = getCurDate;
-Vue.prototype.$setSessionStorage = setSessionStorage;
-Vue.prototype.$getSessionStorage = getSessionStorage;
-Vue.prototype.$removeSessionStorage = removeSessionStorage;
-Vue.prototype.$setLocalStorage = setLocalStorage;
-Vue.prototype.$getLocalStorage = getLocalStorage;
-Vue.prototype.$removeLocalStorage = removeLocalStorage;
 
-router.beforeEach(function(to, from, next){
-	let user = this.sessionStorage.getItem('user');
-	//除了登录、注册、首页、商家列表、商家信息之外，都需要判断是否登录
-	if(!(to.path=='/'||to.path=='/index'||to.path=='/businessList'||to.path=='/businessInfo'||to.path=='/login'||to.path=='/register')){
-		if(user==null){
-			router.push('/login');
-			location.reload;
+// 注册全局属性和方法
+const app = createApp(App);
+app.config.globalProperties.$axios = axios;
+app.config.globalProperties.$getCurDate = getCurDate;
+app.config.globalProperties.$setSessionStorage = setSessionStorage;
+app.config.globalProperties.$getSessionStorage = getSessionStorage;
+app.config.globalProperties.$removeSessionStorage = removeSessionStorage;
+app.config.globalProperties.$setLocalStorage = setLocalStorage;
+app.config.globalProperties.$getLocalStorage = getLocalStorage;
+app.config.globalProperties.$removeLocalStorage = removeLocalStorage;
+
+
+router.beforeEach(function(to, from, next) {
+	let user = getSessionStorage('user'); // 使用getSessionStorage获取用户信息
+	// 除了登录、注册、首页、商家列表、商家信息之外，都需要判断是否登录
+	if (!(to.path == '/' || to.path == '/index' || to.path == '/businessList' || to.path == '/businessInfo' || to.path ==
+			'/login' || to.path == '/register')) {
+		if (user == null) {
+			next('/login'); // 重定向到登录页面
+		} else {
+			next(); // 用户已登录，继续路由跳转
 		}
+	} else {
+		next(); // 其他页面不需要登录验证，直接跳转
 	}
-	next();
 });
 
 createApp(App).use(router).mount('#app')
