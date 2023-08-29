b9aa5b6531186e1fa0585ca07e1b1c46a8154fcb
Sisiliu312 <101872688+Sisiliu312@users.noreply.github.com>
2023-08-29 17:00:58 +0800

    地址页面接口

--- elmclient/src/views/UserAddress.vue
+++ elmclient/src/views/UserAddress.vue
@@ -1,11 +1,40 @@
 <template>
     
+    <!-- 底部菜单部分 -->
+    <Footer></Footer>
 </template>
 
 <script>
+    //导入共通组件
+    import Footer from '../components/Footer.vue';
 
+    export default{
+        name:'UserAddress',
+        data(){
+            return{
+                user:{},
+                deliveryAddressArr:[]
+            }
+        },
+        created(){
+            this.user=this.$getSessionStorage('user');
+
+            //查询送货地址
+            this.$axios.post('DeliveryAddressController/listDeliveryAddressByUserId',this.$qs.stringify({
+				userId:this.user.userId
+			})).then(response=>{
+				this.deliveryAddressArr=response.data;
+			}).catch(error=>{
+				console.error(error);
+			});
+
+        },
+        components:{
+            Footer
+        }
+    }
 </script>
 
-<style>
+<style scoped>
 
 </style>
