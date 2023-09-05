d3d6ccee57319a8d3c4e5b7b23bc385e2bffe4ca
chuckhooker <hook65537@gmail.com>
2023-09-05 17:37:35 +0800

    AI功能 part1

--- elmboot/src/main/java/com/neusoft/elmboot/controller/BusinessController.java
+++ elmboot/src/main/java/com/neusoft/elmboot/controller/BusinessController.java
@@ -20,7 +20,7 @@ public class BusinessController {
 	
 	@RequestMapping("/listBusinessByOrderTypeId")
 	public List<Business> listBusinessByOrderTypeId(Business business) throws Exception{
-		System.out.println("%%%%%%%^^^^^^^^^^");
+		//System.out.println("%%%%%%%^^^^^^^^^^");
 		return businessService.listBusinessByOrderTypeId(business.getOrderTypeId());
 	}
 	
